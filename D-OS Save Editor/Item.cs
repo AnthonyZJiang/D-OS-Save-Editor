b8337d93be703a3a8a6ea195ff5074488ade7f6f
Home Desktop <anthony.zyjiang@outlook.com>
2018-04-01 18:37:32 +0100

    -code clean up.

--- D-OS Save Editor/Item.cs
+++ D-OS Save Editor/Item.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Windows.Media;
 using System.Xml;
 
 namespace D_OS_Save_Editor
--- D-OS Save Editor/LsxParser.cs
+++ D-OS Save Editor/LsxParser.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Xml;
-using LSLib.Granny;
-using LSLib.LS.Story;
 
 namespace D_OS_Save_Editor
 {
